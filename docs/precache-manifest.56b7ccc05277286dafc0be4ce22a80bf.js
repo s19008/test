7396eb79f009e80c8e61089b9ab71813d8539a80
s19008 <s19008@std.it-college.ac.jp>
2020-02-26 06:19:30 +0000

    first commit

---
+++ docs/precache-manifest.56b7ccc05277286dafc0be4ce22a80bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d049f441a4f88e30d1fa45b4763bd322",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "4ffb83ff6d1796bb8a37",
+    "url": "/test/static/css/main.168be87e.chunk.css"
+  },
+  {
+    "revision": "6e5e70c15d1e667b05de",
+    "url": "/test/static/js/2.a511b55e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/test/static/js/2.a511b55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffb83ff6d1796bb8a37",
+    "url": "/test/static/js/main.8cf7a132.chunk.js"
+  },
+  {
+    "revision": "25efeaacbead4a84b640",
+    "url": "/test/static/js/runtime-main.909a2939.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import './App.css'
+import { Button } from '@material-ui/core'
+
+export default class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      c_100: '100.jpg',
+      c_200: '200.jpg',
+      c_403: '403.jpg',
+      c_404: '404.jpg',
+      c_599: '599.jpg',
+      cat: 'https://http.cat/',
+      cat_url: null
+    }
+  }
+
+  URL_cat100 () {
+    this.setState({ cat_url: this.state.cat + this.state.c_100 })
+  }
+
+  URL_cat200 () {
+    this.setState({ cat_url: this.state.cat + this.state.c_200 })
+  }
+
+  URL_cat403 () {
+    this.setState({ cat_url: this.state.cat + this.state.c_403 })
+  }
+
+  URL_cat404 () {
+    this.setState({ cat_url: this.state.cat + this.state.c_404 })
+  }
+
+  URL_cat599 () {
+    this.setState({ cat_url: this.state.cat + this.state.c_599 })
+  }
+
+  render () {
+    return (
+      <div class='button'>
+        <Button
+          onClick={() => this.URL_cat100()}
+          variant='contained'
+          color='primary'
+        >
+          cat 100
+        </Button>
+
+        <Button
+          onClick={() => this.URL_cat200()}
+          variant='contained'
+          color='primary'
+        >
+          cat 200
+        </Button>
+
+        <Button
+          onClick={() => this.URL_cat403()}
+          variant='contained'
+          color='primary'
+        >
+          cat 403
+        </Button>
+
+        <Button
+          onClick={() => this.URL_cat404()}
+          variant='contained'
+          color='primary'
+        >
+          cat 404
+        </Button>
+
+        <Button
+          onClick={() => this.URL_cat599()}
+          variant='contained'
+          color='primary'
+        >
+          cat 599
+        </Button>
+
+        <img src={this.state.cat_url}></img>
+      </div>
+    )
+  }
+}
